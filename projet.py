d277988bc17b0637ad60648ae987771b7d876ee6
Hamdi Aziz <lolaziz.ca@gmail.com>
2025-07-09 19:31:06 +0200

    fussioner 2 commit en une seul

--- projet.py
+++ projet.py
@@ -4,4 +4,9 @@ def produit(T):
         s*=t
         return S
 Data=[1,3,5]
-Prod=math.prod(Data) print('le produit est :',Prod)
+if Data :
+    print('le produit est :',Prod(Data))
+    print('le min est :',min(Data))
+    print('le max est :',max(Data))
+else :
+    print('dossier vide')
